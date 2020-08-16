9a4ba6614a61337d874916ace8207a009064521a
Ahmed Munir <ahmedmunir7073@gmail.com>
2020-08-16 22:51:17 +0500

    Project Front End

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $nav=$('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+$toggleCollapse.click(function(){
+    $nav.toggleClass('collapse'); 
+});
+
+const responsive = {
+    0: {
+        items:1
+    },
+    320 : {
+        items:1
+    },
+    560 :{
+        items:2
+    },
+    960 :{
+        items:3
+    }
+}
+
+//owl-carousel for blog
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+});
+
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop:0
+    },3000)
+});
+
+
+var scrollBottom = $(window).scrollTop() + $(window).height();
+
+$('btn span').click(function(){
+    $('html,body').animate({scrollTop: $(document).height() + $(window).height()});
+});
+
+
+
+
+});
